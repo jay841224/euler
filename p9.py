37a6d065b9edc1968dc202c5d938021262490b49
jay841224 <j1360fy6416nb@gmail.com>
2019-09-24 22:19:53 +0800

    initial

---
+++ p9.py
@@ -0,0 +1,15 @@
+#There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+a = b = c = 0
+s = 1000
+ans = []
+while(a<s/3):
+    while(b<s/2):
+        c = s - a - b
+        if(a<b<c and a**2+b**2==c**2):
+            ans.append(a*b*c)
+        b += 1
+    a += 1
+    b = a
+ 
+print(ans)
