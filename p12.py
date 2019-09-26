40ca37953d06019735c5bd955dfc1445636918b8
jay841224 <j1360fy6416nb@gmail.com>
2019-09-26 13:07:13 +0800

    finished

--- p12.py
+++ p12.py
@@ -1,24 +1,28 @@
 #What is the value of the first triangle number to have over five hundred divisors?
-def triangle_numberOf_divisors(divisor_amount):
-    number = 3
-    i = 3
-    count_divisor = 2
-    a_2 = 0
-    while count_divisor<divisor_amount:
-        a = 2
-        count_divisor = 2
-        while number > a:
-            if number%a == 0:
-                if a_2 == number:
-                    count_divisor += 1
-                else:
-                    count_divisor += 2
-            a += 1
-            a_2 = a**2
-            if a_2 > number:
-                break
+def find_divisorOf_triangle(number_divisor):
+    i = 2
+    number = 1
+    divisors_count = 0
+    while divisors_count < number_divisor:
         number += i
         i += 1
+        a = 2
+        x = number
+        divisors = []
+        while x >= a:
+            if x%a == 0:
+                x /= a
+                divisors.append(a)
+            else:
+                a += 1
+        temp_divisors_count = 1
+        while len(divisors) > 0:
+            count = divisors.count(divisors[0])
+            temp_divisors_count *= (count + 1)
+            kill = divisors[0]
+            while count > 0:
+                divisors.remove(kill)
+                count -= 1
+        divisors_count = temp_divisors_count
     return number
-ans = triangle_numberOf_divisors(500)
-print(ans)
+print(find_divisorOf_triangle(500))
